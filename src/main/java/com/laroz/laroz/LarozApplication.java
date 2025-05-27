e07ef7dec6c499aff8a046d55bd03faaa4bd7443
Elias Moura <elias.smourayt@gmail.com>
2025-05-26 22:03:44 -0300

    Commit inicial: configurações básicas do ambiente com docker, pom.xml e application.yml

---
+++ src/main/java/com/laroz/laroz/LarozApplication.java
@@ -0,0 +1,13 @@
+package com.laroz.laroz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LarozApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LarozApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/laroz/laroz/LarozApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laroz.laroz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LarozApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
